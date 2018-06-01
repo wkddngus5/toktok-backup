f27326e4067f39e0d3b7c9b98e62280934e93908
wkddngus5 <wkddngus5@naver.com>
2018-06-01 14:47:00 +0900

    just for backup

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,10 +11,22 @@ class Header extends Component {
     return (
       <header className="header">
         <a>국회톡톡</a>
-        <div className="right-elements">
-          <div className="search-zone">SEARCH</div>
-          <div className="menu" style={menuStyle}></div>
-        </div>
+        <button id="demo-menu-lower-right"
+                className="mdl-button mdl-js-button mdl-button--icon">
+          <i className="material-icons">more_vert</i>
+        </button>
+
+        <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+            for="demo-menu-lower-right">
+          <li className="mdl-menu__item">Some Action</li>
+          <li className="mdl-menu__item">Another Action</li>
+          <li disabled className="mdl-menu__item">Disabled Action</li>
+          <li className="mdl-menu__item">Yet Another Action</li>
+        </ul>
+        {/*<div className="right-elements">*/}
+          {/*<div className="search-zone">SEARCH</div>*/}
+          {/*<div className="menu" style={menuStyle}></div>*/}
+        {/*</div>*/}
       </header>
     )
   }
